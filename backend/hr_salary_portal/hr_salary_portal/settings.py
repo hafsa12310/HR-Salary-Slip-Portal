5ed3f826f9ccf0920a3f9df3a6ddb2646218f722
hafsa12310 <hafsa.172001@gmail.com>
2024-08-19 20:56:22 +0500

    gitignore added

--- backend/hr_salary_portal/hr_salary_portal/settings.py
+++ backend/hr_salary_portal/hr_salary_portal/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'payroll',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +65,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 
 ROOT_URLCONF = 'hr_salary_portal.urls'
 
